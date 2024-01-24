12136a5cc544eab742d220511537be930e960000
David Silva <david_goncalvessilva@hotmail.com>
2024-01-24 10:26:01 +0100

    Add properties to Comment Model

--- api/models/Comment.cs
+++ api/models/Comment.cs
@@ -7,7 +7,17 @@ namespace api.models
 {
     public class Comment
     {
+        #region Database Properties
+        public Guid CommentId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreationDateTime { get; set; } = DateTime.Now;
+
         public Guid? StockId { get; set; }
+        #endregion
 
         #region Navigation Properties
         public Stock? Stock { get; set; }
